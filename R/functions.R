8c9c151a5e5e69535ff93c2900e31cdde1caa8d6
Nicole Jacqueline Jensen <nicole.jacqueline.jensen@regionh.dk>
2023-11-09 15:38:25 +0100

    update recipe specifications

--- R/functions.R
+++ R/functions.R
@@ -5,10 +5,10 @@
 #' @return A data.frame/tibble.
 
 descriptive_stats <- function(data) {
-    data %>%
-        dplyr::group_by(metabolite) %>%
-        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
 
 #' Plot for basic distribution of metabolite data.
@@ -18,8 +18,56 @@ descriptive_stats <- function(data) {
 #' @return A ggplot2 graph.
 
 plot_distributions <- function(data) {
+  data %>%
+    ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free")
+}
+
+#' Convert column value strings into snakecase.
+#'
+#' @param data Data with string columns.
+#' @param cols The column to convert into snakecase.
+#'
+#' @return A data frame.
+
+column_values_to_snake_case <- function(data, cols) {
+  data %>%
+    dplyr::mutate(dplyr::across({{ cols }}, snakecase::to_snake_case))
+}
+
+
+#' Convert the metabolite long format into a wider one.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A wide data frame.
+
+
+metabolites_to_wider <- function(data) {
     data %>%
-        ggplot2::ggplot(ggplot2::aes(x = value)) +
-        ggplot2::geom_histogram() +
-        ggplot2::facet_wrap(vars(metabolite), scales = "free")
+        tidyr::pivot_wider(
+            names_from = metabolite,
+            values_from = value,
+            values_fn = mean,
+            names_prefix = "metabolite_"
+        )
+
+}
+
+
+#' A transformation recipe to pre-process the data.
+#'
+#' @param data The lipidomics dataset.
+#' @param metabolite_variable The column of the metabolite variable.
+#'
+#' @return Recipe with specific functions
+
+create_recipe_spec <- function(data, metabolite_variable) {
+    recipes::recipe(data) %>%
+        recipes::update_role({{metabolite_variable}},
+                            age, gender, new_role = "predictor") %>%
+        recipes::update_role(class, new_role = "outcome") %>%
+        recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+
 }
