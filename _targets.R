703301a2c2e885b50bf3d1d0373ec78bcf6ad89d
Nicole Jacqueline Jensen <nicole.jacqueline.jensen@regionh.dk>
2023-11-10 13:45:49 +0100

    include freeze

--- _targets.R
+++ _targets.R
@@ -67,8 +67,8 @@ list(
     command = plot_distributions(lipidomics)
   ),
   tar_quarto(
-    name = quarto_doc,
-    path = "doc/learning.qmd"
+    name = quarto_website,
+    path = "." #means the same folder as _targets.R is in
   ),
   tar_target(
     name = df_model_estimates,
